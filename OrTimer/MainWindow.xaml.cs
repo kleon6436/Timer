fba99d7c2f8368e12d135817f1c8df0d409ef5f0
中浜圭貴 <kn1845@gmail.com>
2018-09-29 21:43:31 +0900

    OｒTimerの大枠を実装。MVVMによる制御とする。

--- OrTimer/MainWindow.xaml.cs
+++ OrTimer/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OrTimer.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OrTimer
 {
@@ -23,6 +11,9 @@ namespace OrTimer
         public MainWindow()
         {
             InitializeComponent();
+
+            MainWindowViewModel _mainWindowViewModel = new MainWindowViewModel();
+            this.DataContext = _mainWindowViewModel;
         }
     }
 }
---
+++ OrTimer/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,94 @@
+﻿using Prism.Mvvm;
+using Prism.Commands;
+using System.Windows.Input;
+
+namespace OrTimer.ViewModel
+{
+    public class MainWindowViewModel:BindableBase
+    {
+        // コマンド一覧
+        public ICommand MinutePlusCommand { get; set; }
+        public ICommand MinuteMinusCommand { get; set; }
+        public ICommand SecondPlusCommand { get; set; }
+        public ICommand SecondMinusCommand { get; set; }
+        public ICommand StartButtonCommand { get; set; }
+        public ICommand StopButtonCommand { get; set; }
+        public ICommand ResetButtonCommand { get; set; }
+
+        // 値のプロパティ一覧
+        private string _minuteText;
+        public string MinuteText
+        {
+            get { return _minuteText; }
+            set { SetProperty(ref _minuteText, value); }
+        }
+
+        private string _secondText;
+        public string SecondText
+        {
+            get { return _secondText; }
+            set { SetProperty(ref _secondText, value); }
+        }
+
+        // コンストラクタ
+        public MainWindowViewModel()
+        {
+            // コマンドの設定
+            SetCommand();
+
+            // プロパティの初期値をセット
+            const string _defaultValue = "00";  // ２桁表示であることに注意
+            MinuteText = _defaultValue;
+            SecondText = _defaultValue;
+        }
+
+        /// <summary>
+        /// コマンドの設定
+        /// </summary>
+        private void SetCommand()
+        {
+            MinutePlusCommand = new DelegateCommand(MinutePlusCommandClicked);
+            MinuteMinusCommand = new DelegateCommand(MinuteMinusCommandClicked);
+            SecondPlusCommand = new DelegateCommand(SecondPlusCommandClicked);
+            SecondMinusCommand = new DelegateCommand(SecondMinusCommandClicked);
+            StartButtonCommand = new DelegateCommand(StartButtonCommandClicked);
+            StopButtonCommand = new DelegateCommand(StopButtonCommandClicked);
+            ResetButtonCommand = new DelegateCommand(ResetButtonCommandClicked);
+        }
+
+        private void MinutePlusCommandClicked()
+        {
+
+        }
+
+        private void MinuteMinusCommandClicked()
+        {
+
+        }
+
+        private void SecondPlusCommandClicked()
+        {
+
+        }
+
+        private void SecondMinusCommandClicked()
+        {
+
+        }
+
+        private void StartButtonCommandClicked()
+        {
+
+        }
+
+        private void StopButtonCommandClicked()
+        {
+
+        }
+
+        private void ResetButtonCommandClicked()
+        {
+
+        }
+    }
+}
